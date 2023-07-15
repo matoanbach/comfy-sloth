31455c4a13f54416787804386281afb19f598933
toan <bachmatoan2910@gmail.com>
2023-07-15 13:54:16 -0400

    Set up and add functionality to  the navbar, sidebar and style the error page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes,
+} from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+import styled from "styled-components";
+
+import {
+  Home,
+  About,
+  AuthWrapper,
+  Cart,
+  Checkout,
+  Error,
+  PrivateRoute,
+  Products,
+  SingleProduct,
+} from "./pages/index";
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar/>
+      <Routes>
+        <Route index element={<Home/>}></Route>
+        <Route path="about" element={<About/>}></Route>
+        <Route path="products" element={<Products/>}></Route>
+        <Route path="cart" element={<Cart/>}></Route>
+        <Route path="checkout" element={<Checkout/>}></Route>
+        <Route path="*" element={<Error/>}></Route>
+      </Routes>
+      <Footer/>
+    </Router>
+  );
 }
 
-export default App
+export default App;
